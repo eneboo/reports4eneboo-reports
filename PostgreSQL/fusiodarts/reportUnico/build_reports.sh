a68ede9597cf37927c1c55ce3f40ec12c42a2f39
Angel Albiach (fusiodarts) <angel@fusiodarts.com>
2014-06-27 13:45:50 +0200

    fusiodarts reportUnico: Reports de proveedores
    Nuevos reports de facturación de proveedores.

--- PostgreSQL/fusiodarts/reportUnico/build_reports.sh
+++ PostgreSQL/fusiodarts/reportUnico/build_reports.sh
@@ -1,13 +1,17 @@
 #/bin/sh
-# Reports Facturación
+# Reports Facturación Clientes
 cp reportUnico.jrxml reports/i_presupuestoscli/i_presupuestoscli.jrxml
-cp reportUnico.jrxml reports/i_pedidoscli/i_pedidoscli.jrxml 
-cp reportUnico.jrxml reports/i_albaranescli/i_albaranescli.jrxml 
+cp reportUnico.jrxml reports/i_pedidoscli/i_pedidoscli.jrxml
+cp reportUnico.jrxml reports/i_albaranescli/i_albaranescli.jrxml
 cp reportUnico.jrxml reports/i_facturascli/i_facturascli.jrxml
+# Reports Facturación Proveedores
+cp reportUnico.jrxml reports/i_pedidosprov/i_pedidosprov.jrxml
+cp reportUnico.jrxml reports/i_albaranesprov/i_albaranesprov.jrxml
+cp reportUnico.jrxml reports/i_facturasprov/i_facturasprov.jrxml
 # Reports Resumenes
 cp resumenUnico.jrxml reports/i_respresupuestoscli/i_respresupuestoscli.jrxml
-cp resumenUnico.jrxml reports/i_respedidoscli/i_respedidoscli.jrxml 
-cp resumenUnico.jrxml reports/i_resalbaranescli/i_resalbaranescli.jrxml 
+cp resumenUnico.jrxml reports/i_respedidoscli/i_respedidoscli.jrxml
+cp resumenUnico.jrxml reports/i_resalbaranescli/i_resalbaranescli.jrxml
 cp resumenUnico.jrxml reports/i_resfacturascli/i_resfacturascli.jrxml
 # Estilo
 cp style.jrtx reports/style.jrtx
